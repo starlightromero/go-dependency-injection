2825a763a81dc736e91281b1656aa93d9a476861
Starlight Romero <starlightromero@protonmail.com>
2021-04-19 14:11:04 -0700

    [update] added simple server to expand functionality of writer

--- di.go
+++ di.go
@@ -1,15 +1,20 @@
 package main
 
 import (
-	"os"
+	"log"
 	"io"
 	"fmt"
+	"net/http"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world")
+}
+
 func main() {
-	Greet(os.Stdout, "Sid")
+	log.Fatal(http.ListenAndServe(":5000", http.HandlerFunc(MyGreeterHandler)))
 }
